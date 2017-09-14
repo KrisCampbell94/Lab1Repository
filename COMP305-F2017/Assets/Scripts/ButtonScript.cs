67f4f0603d51713f617361ac7c951fe464351ebe
Kristian Campbell <kriscampbell_94@yahoo.com>
2017-09-13 22:45:42 -0400

    Lab2

---
+++ COMP305-F2017/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public float buttonNumber;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+    }
+    private void OnMouseDown()
+    {
+        ClothingChangeScript.styleChange = buttonNumber;
+        Debug.Log("Left Click by " + buttonNumber);
+    }
+}
---
+++ COMP305-F2017/Assets/Scripts/ClothingChangeScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothingChangeScript : MonoBehaviour {
+    //private GameObject buttonA;
+    public static float styleChange = 1f;
+
+    public Sprite clothingA;
+    public Sprite clothingB;
+    public Sprite clothingC;
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (styleChange == 1)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = clothingA;
+            Debug.Log("A");
+        }
+        else if (styleChange == 2)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = clothingB;
+            Debug.Log("B");
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = clothingC;
+            Debug.Log("C");
+        }
+    }
+}
